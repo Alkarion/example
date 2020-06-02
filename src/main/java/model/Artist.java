6b8adf0094dc364ce493ba8ce36d7637f3442c35
Princess <kaserek99@vp.pl>
2020-06-02 20:45:58 +0200

    2nd Module

--- src/main/java/model/Artist.java
+++ src/main/java/model/Artist.java
@@ -1,61 +1,26 @@
-package model;
+package repcak.example.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
 public class Artist {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-@ManyToMany(mappedBy = "artists")
+    private String firstName;
+    private String lastName;
+    private String nick;
 
-private Long id;
-private String firstName;
-private String lastName;
-private String nick;
-private Set<Song> songs = new HashSet<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public Set<Song> getSongs() {
-        return songs;
-    }
-
-    public void setSongs(Set<Song> songs) {
-        this.songs = songs;
-    }
+    @ManyToMany(mappedBy = "artists")
+    private Set<Song> songs = new HashSet<>();
 
     public Artist() {
     }
@@ -72,14 +37,4 @@ private Set<Song> songs = new HashSet<>();
         this.songs = songs;
     }
 
-    @Override
-    public String toString() {
-        return "Artist{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", nick='" + nick + '\'' +
-                ", songs=" + songs +
-                '}';
-    }
 }
--- src/main/java/model/Song.java
+++ src/main/java/model/Song.java
@@ -1,81 +1,30 @@
-package model;
+package repcak.example.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(of = {"id"})
 @Entity
 public class Song {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-@ManyToMany
+    private String title;
+    private String genre;
+    private String ismn;
+    private String year;
+    private String publisher;
 
+    @ManyToMany
+    private Set<Artist> artists = new HashSet<>();
 
-private Long id;
-private String title;
-private String genre;
-private String ismn;
-private String year;
-private String publisher;
-private Set<Artist> artists = new HashSet<>();
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getIsmn() {
-        return ismn;
-    }
-
-    public void setIsmn(String ismn) {
-        this.ismn = ismn;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Set<Artist> getArtists() {
-        return artists;
-    }
-
-    public void setArtists(Set<Artist> artists) {
-        this.artists = artists;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public Song() {
     }
 
     public Song(String title, String genre, String ismn, String year, String publisher) {
@@ -93,22 +42,5 @@ private Set<Artist> artists = new HashSet<>();
         this.year = year;
         this.publisher = publisher;
         this.artists = artists;
-    }
-
-
-    public Song() {
-    }
-
-    @Override
-    public String toString() {
-        return "Song{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", genre='" + genre + '\'' +
-                ", ismn='" + ismn + '\'' +
-                ", year='" + year + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", artists=" + artists +
-                '}';
     }
 }
